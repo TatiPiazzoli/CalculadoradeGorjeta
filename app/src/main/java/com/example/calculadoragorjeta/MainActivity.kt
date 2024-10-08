302fdf649d383b0880d4a7789405c8beb7cbfc4c
TatiPiazzoli <tatianamenpia@gmail.com>
2024-10-08 19:23:51 +0100

    Começando binding

--- app/src/main/java/com/example/calculadoragorjeta/MainActivity.kt
+++ app/src/main/java/com/example/calculadoragorjeta/MainActivity.kt
@@ -1,20 +1,45 @@
 package com.example.calculadoragorjeta
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.calculadoragorjeta.databinding.ActivityMainBinding
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+        
+        binding.rbTen.setOnCheckedChangeListener { _, isChecked ->
+
+        }
+
+        binding.rbFifteen.setOnCheckedChangeListener { _, isChecked ->
+
+        }
+
+        binding.rbTwenty.setOnCheckedChangeListener { _, isChecked ->
+
+        }
+
+        binding.btnClean.setOnClickListener {
+
+        }
+
+        binding.btnCalculate.setOnClickListener {
+
+            val Total: Float = binding.total.text.toString().toFloat()
+            val People: Float = binding.people.text.toString().toFloat()
+
         }
     }
 }
